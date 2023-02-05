063f35407e4b6d2c60ab6d0fe195dc33f3ab54ce
Ryan Kunkel <rykunk21@gmail.com>
2023-02-05 11:52:34 -0500

    changed

---
+++ test/preproccesor.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestPreprocessor(unittest.TestCase):
+
+    def test_init(self):
+        self.assertRaises(Exception, lambda: (_ for _ in ()).throw(Exception('Exception')))
+
+    def test_drop(self):
+        pass
+
+    def test_adjust(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
